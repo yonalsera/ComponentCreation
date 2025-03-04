0d0ffafa283430bfd1c07cef23f12002947cb837
yonalsera <yonal.serasinghe@gmail.com>
2025-03-03 19:31:08 -0500

    Established interfaces

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -76,3 +76,13 @@ the following form: YYYY.0M.0D.
 [unreleased]: https://github.com/jrg94/portfolio-project/compare/v2024.08.07...HEAD
 [2024.08.07]: https://github.com/jrg94/portfolio-project/compare/v2024.01.07...v2024.08.07
 [2024.01.07]: https://github.com/jrg94/portfolio-project/releases/tag/v2024.01.07
+
+## [2025.03.03]
+
+### Added
+
+- Designed kernel and enhanced interfaces for MusicPlaylist component
+
+### Updated
+
+- Changed design to include thorougly established interfaces for the kernel and secondary methods within MusicPlaylist, more specifically the return type and parameters.
---
+++ src/MusicPlaylist.java
@@ -0,0 +1,44 @@
+import components.map.Map;
+import components.sequence.Sequence;
+
+/**
+ * {@code MusicPlaylistKernel} enhanced with secondary methods.
+ */
+public interface MusicPlaylist extends MusicPlaylistKernel {
+    /**
+     * Merge two playlists together.
+     *
+     * @param playlist
+     *            playlist to be checked
+     * @param p2
+     *            The second playlist to be merged to the first
+     */
+    void merge(Sequence<Map.Pair<String, String>> playlist,
+            Sequence<Map.Pair<String, String>> p2);
+
+    /**
+     * Find and return the artist of the given song.
+     *
+     * @param playlist
+     *            playlist to be checked
+     * @param title
+     *            Name of the song to find the corresponding artst
+     * @return String Name of the artist
+     */
+    String getArtist(Sequence<Map.Pair<String, String>> playlist, String title);
+
+    /**
+     * Randomly shuffle the order of songs in the playlist.
+     */
+    void shuffle();
+
+    /**
+     * Returns a path to the given song in URL format.
+     *
+     * @param title
+     *            Title of song that will have its path returned
+     * @return String path to song in URL format
+     */
+    String getPath(String title);
+
+}
