da9890d240a870d6ee9223962ecf8a610c04dc7f
yonalsera <yonal.serasinghe@gmail.com>
2025-04-08 18:31:26 -0400

    method parameter change

--- src/MusicPlaylist.java
+++ src/MusicPlaylist.java
@@ -8,24 +8,19 @@ public interface MusicPlaylist extends MusicPlaylistKernel {
     /**
      * Merge two playlists together.
      *
-     * @param playlist
-     *            playlist to be checked
      * @param p2
      *            The second playlist to be merged to the first
      */
-    void merge(Sequence<Map.Pair<String, String>> playlist,
-            Sequence<Map.Pair<String, String>> p2);
+    void merge(Sequence<Map.Pair<String, String>> p2);
 
     /**
      * Find and return the artist of the given song.
      *
-     * @param playlist
-     *            playlist to be checked
      * @param title
      *            Name of the song to find the corresponding artst
      * @return String Name of the artist
      */
-    String getArtist(Sequence<Map.Pair<String, String>> playlist, String title);
+    String getArtist(String title);
 
     /**
      * Randomly shuffle the order of songs in the playlist.
